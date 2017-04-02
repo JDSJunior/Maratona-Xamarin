8ecc9898622e1500e8d123526aaf4bf6caae2b3a
Júnior Silva <junior_dariana@hotmail.com>
2017-04-02 18:56:21 -0300

    7° commit

---
+++ Greetings/Greetings/Greetings/GreetingPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public class GreetingPage : ContentPage
+    {
+        public GreetingPage()
+        {
+            var Mylabel = new Label();
+            Mylabel.Text = "Greetings, xamarin.forms!";
+            this.Content = Mylabel;
+
+            Mylabel.HorizontalOptions = LayoutOptions.Center;
+            Mylabel.VerticalOptions = LayoutOptions.Center;
+
+            Mylabel.HorizontalTextAlignment = TextAlignment.Center;
+            Mylabel.VerticalTextAlignment = TextAlignment.Center;
+
+            Mylabel.TextColor = Color.Aqua;
+        }
+    }
+}
